8c2e77163cb6027fb99189ce51905dbb80b6032b
Ayush <ayushpandya628@gmail.com>
2026-04-27 12:50:46 -0400

    update borrow book with sql

--- member.py
+++ member.py
@@ -1,65 +1,78 @@
 from books import Book
-import email_validator
-from library import *
+import email_validator as emv
+from library import library
 from exception import *
 class Member:
-    def __init__(self, name: str, age: int, email: str, library_id: int):
-        self.__name = name
-        self.__age = age
-        self.__email = email_validator.validate_email(email, check_deliverability= False).email
-        self.__library_id = library_id
-        self.__books_borrowed = set()
-        self.__num_borrowed = 0
+    def __init__(self, firstN: str, lastN : str,  age: int, email: str):
+        self.connection = get_connection()
+        cursor = self.connection.cursor()
+        self.__First_Name, self.__Last_Name, self.__Age, self.__Email =  firstN, lastN, age, emv.validate_email(email, check_deliverability=False).email
+        cursor.execute("insert into Member (First_Name, Last_Name, Age, Email) values (?,?,?,?)", ( firstN, lastN, age, self.__Email))
+        self.__ID = cursor.lastrowid
+        self.connection.commit()
+        cursor.close()
 
     @property
-    def name(self)-> str:
-        return self.__name
+    def first_name(self)-> str:
+        return self.__First_Name
     
     @property
+    def last_name(self)-> str:
+        return self.__Last_Name
+
+    @property
     def age(self) -> int:
-        return self.__age
+        return self.__Age
     
     @property
     def email(self) -> str:
-        return self.__email
+        return self.__Email
     
     @property
-    def library_id(self)-> int:
-        return self.__library_id
+    def id(self):
+        return self.__ID
     
-    @name.setter
-    def name(self, name: str)-> None:
-        self.__name = name
+    @first_name.setter
+    def first_name(self, first: str)-> None:
+        self.__First_Name = first
+
+    @last_name.setter
+    def last_name(self, last: str)-> None:
+        self.__Last_Name = last
 
     @age.setter
     def age(self, age: int) -> None:
-        self.__age = age
+        self.__Age = age
     
     @email.setter
     def email(self, email: str) -> None:
-        self.__email = email_validator.validate_email(email, check_deliverability= False).email
+        self.__Email = emv.validate_email(email, check_deliverability= False).email
 
     # Library id, once created, can not be changed so no setter for it 
 
-    def print_set(self) -> None:
-        for x in self.__books_borrowed:
-            print(x)
 
-    def borrow_book(self, book: Book, lib: library) -> None:
-        if book in lib.book_list:
-            self.__books_borrowed.add(book)
-            self.__num_borrowed+=1
-            lib.removeBook_Book(book)
-        else:
-            raise searchError("Book is not present in the library")
+    def borrow_book(self, book: Book, lib : library) -> None:
+        cursor = self.connection.cursor()
+        try:
+            temp = lib.getBook_by_serialNumber(book.serialNumber)
+            if temp[6] == 0:
+                print("Book has already been borrrowed")
+            elif temp[6] == 1:
+                cursor.execute("update Books set is_borrowed = 1 where serial_number = ?", (book.serialNumber,))
+                cursor.execute("insert into Borrows(member_id, book_id) values (?,?)", (self.__ID, book.serialNumber))
+                self.connection.commit()
+                cursor.close()
+                print("The book has been borrowed")
+        except BookRetrivalError:
+            raise BookRetrivalError("The book is not present in the library")
 
     def return_book(self, book: Book, lib: library) -> None:
-        if book in self.__books_borrowed:
-            self.__books_borrowed.discard(book)
-            self.__num_borrowed -= 1
-            lib.addbook(book)
-        else:
-            raise searchError("Book not present to return")
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select * from Borrows where book_id = ?", (book.serialNumber))
+        except:
+            pass
+
 
     def __str__(self) -> str:
         return(f"Member name is {self.__name} with {self.__num_borrowed} borrowed book(s)")
